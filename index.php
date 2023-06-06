03db269e1bcaad27325f9eca85c2acda518a55c7
LucasHenriqueSS <l.henriquess01@gmail.com>
2023-06-06 10:28:30 -0300

    autoload hcodebr classes

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-echo "OK! git iniado";
+require_once("vendor/autoload.php");
 
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function(){
+
+	$sql = new Hcode\DB\Sql();
+	$results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
+
+
+});
+
+$app->run();
 
 ?>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit9362873f2d808fb7cf917b951d0de77c
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInit9362873f2d808fb7cf917b951d0de77c
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit9362873f2d808fb7cf917b951d0de77c::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit9362873f2d808fb7cf917b951d0de77c::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit9362873f2d808fb7cf917b951d0de77c::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit9362873f2d808fb7cf917b951d0de77c::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit9362873f2d808fb7cf917b951d0de77c::$classMap;
 
